3c7d3fa9790351aceece2d7f31721e0344565f21
duck <113956421+duckfromdiscord@users.noreply.github.com>
2023-12-01 22:41:21 -0500

    we don't really need to write test outputs, and it also causes a panic if we can't

--- restruct_serialization/tests/passthrough_tests.rs
+++ restruct_serialization/tests/passthrough_tests.rs
@@ -1,12 +1,12 @@
 use std::str;
 use std::fs;
 
-fn passthrough(xml: &str, testname: &str) {
+fn passthrough(xml: &str, _testname: &str) {
     let structs = restruct_serialization::serialize::structs_from_string( xml.to_string() );
     let new_bytes = restruct_serialization::deserialize::structs_to_bytes(&structs.unwrap());
     let new_xml = str::from_utf8( &new_bytes ).unwrap();
     
-    fs::write("../testoutputs/".to_owned() + &testname + ".xml", new_xml).unwrap();
+    //fs::write("../testoutputs/".to_owned() + &testname + ".xml", new_xml).unwrap();
 
     assert_eq!(xml, new_xml);
 }
